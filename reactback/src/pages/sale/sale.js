cdc1b1f507d1beec2d35cb54182bf71ec2e242c6
li-junjun <54562406+li-junjun@users.noreply.github.com>
2020-01-07 21:06:48 +0800

    345

--- reactback/src/pages/sale/sale.js
+++ reactback/src/pages/sale/sale.js
@@ -1,11 +1,57 @@
-import React from 'react';
-
-function Sale(){
-    return(
-        <div className='login'>
-          数据统计表并图
-        </div>
-    )
+import React,{Fragment,Component} from 'react';
+import ReactEcharts from 'echarts-for-react';
+import './sale.css'
+class Sale extends Component{
+  constructor(){
+    super()
+    this.state={
+      option : {
+        title: {
+            text: '某站点用户访问来源',
+            subtext: '纯属虚构',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+        },
+        series: [
+            {
+                name: '访问来源',
+                type: 'pie',
+                radius: '55%',
+                center: ['50%', '60%'],
+                data: [
+                    {value: 335, name: '直接访问'},
+                    {value: 310, name: '邮件营销'},
+                    {value: 234, name: '联盟广告'},
+                    {value: 135, name: '视频广告'},
+                    {value: 1548, name: '搜索引擎'}
+                ],
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+      }
+    }
+  }
+    render(){
+      let {option} = this.state
+      return(
+          <div className='login'>
+            <ReactEcharts option={option} className='box'></ReactEcharts>
+          </div>
+      )
+  }
 }
-
 export default Sale;
