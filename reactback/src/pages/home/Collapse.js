877ad9c1d1e99e0a6c66cf5c05d904b3c04ae618
郭晓强 <guoxm0204@qq.com>
2020-01-11 10:01:55 +0800

    123

---
+++ reactback/src/pages/home/Collapse.js
@@ -0,0 +1,58 @@
+import React,{Component} from 'react'
+import { Alert } from 'antd';
+const onClose = e => {
+    console.log(e, 'I was closed.');
+  };
+class Collapse extends Component {
+  
+  
+    render() {
+      return (
+        <div>
+    <Alert message="Success Tips" type="success" showIcon />
+    <Alert message="Informational Notes" type="info" showIcon />
+    <Alert message="Warning" type="warning" showIcon />
+    <Alert message="Error" type="error" showIcon />
+    <Alert
+      message="Success Tips"
+      description="Detailed description and advice about successful copywriting."
+      type="success"
+      showIcon
+    />
+    <Alert
+      message="Informational Notes"
+      description="Additional description and information about copywriting."
+      type="info"
+      showIcon
+    />
+    <Alert
+      message="Warning"
+      description="This is a warning notice about copywriting."
+      type="warning"
+      showIcon
+    />
+    <Alert
+      message="Error"
+      description="This is an error message about copywriting."
+      type="error"
+      showIcon
+    />
+    <Alert
+      message="Warning Text Warning Text Warning TextW arning Text Warning Text Warning TextWarning Text"
+      type="warning"
+      closable
+      onClose={onClose}
+    />
+    <Alert
+      message="Error Text"
+      description="Error Description Error Description Error Description Error Description Error Description Error Description"
+      type="error"
+      closable
+      onClose={onClose}
+    />
+  </div>
+      );
+    }
+  }
+  
+export default Collapse
--- reactback/src/pages/home/home.js
+++ reactback/src/pages/home/home.js
@@ -1,11 +1,110 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styles from './home.module.less'
+import {Menu, Dropdown,Icon ,Layout} from 'antd'
 
-function Home(){
-    return(
-        <div className='login'>
-            首页
+import Pie from '../../components/echarts/pie/pie'
+import Bar from '../../components/echarts/bar/bar'
+import Comment from './Comment'
+import Collapse from './Collapse'
+const { Header, Content, Footer, Sider } = Layout;
+class Home extends Component{
+    render(){
+        return(
+        <div className={styles.home}>
+            <Layout>
+            <Content style={{ margin: '24px 16px 0'}} scroll={{y:200,x:500}}>
+            <div className={styles.user}>
+            <Icon type="user" className={styles.userIcon} />
+                <span>321</span>
+                <h3>新增客户</h3>
+            </div>
+            <div className={styles.tags}>
+            <Icon type="tags" className={styles.tagsIcon} />
+                <span>+970</span>
+                <h3>销售数据</h3>
+            </div>
+            <div className={styles.wechat}>
+            <Icon type="wechat" className={styles.wechatIcon} />
+                <span>49</span>
+                <h3>用户评论</h3>
+            </div>
+            <div className={styles.eye}>
+            <Icon type="eye" className={styles.eyeIcon} />
+                <span>+897</span>
+                <h3>UV</h3>
+            </div>
+            <div className={styles.chart}>
+            <Icon type="bar-chart" className={styles.chartIcon} />
+                <span>+288</span>
+                <h3>更新</h3>
+            </div>
+            <div className={styles.shopping}>
+            <Icon type="shopping-cart" className={styles.shoppingIcon} />
+                <span>29</span>
+                <h3>新增订单</h3>
+            </div>
+            <div className={styles.database}>
+            <Icon type="database" className={styles.databaseIcon} />
+                <span>$37624</span>
+                <h3>库存信息</h3>
+            </div>
+            <div className={styles.mail}>
+            <Icon type="mail" className={styles.mailIcon} />
+                <span>123</span>
+                <h3>消息</h3>
+            </div>
+            <div className={styles.close}>
+            <Icon type="close-circle" className={styles.closeIcon} />
+                <span>34</span>
+                <h3>取消订单</h3>
+            </div>
+            <div className={styles.transaction}>
+            <Icon type="transaction" className={styles.transactionIcon} />
+                <span>$53412</span>
+                <h3>总利润</h3>
+            </div>
+        
+        <div className={styles.pie}>
+            <div className={styles.sliver}>
+            <Icon type="menu" />
+            饼状图
+            </div>
+            <div className={styles.piee}>
+                <Pie></Pie>
+            </div>
+        </div>
+        <div className={styles.pie}>
+            <div className={styles.sliver}>
+            <Icon type="menu" />
+            柱状图
+            </div>
+            <div className={styles.piee}>
+                <Bar></Bar>
+            </div>
+        </div>
+        <div className={styles.pie}>
+            <div className={styles.sliver}>
+            <Icon type="menu" />
+            评论信息
+            </div>
+            <div className={styles.piee}>
+                <Comment></Comment>
+            </div>
+        </div>
+        <div className={styles.pie}>
+            <div className={styles.sliver}>
+            <Icon type="menu" />
+            警告提示
+            </div>
+            <div className={styles.piee}>
+                <Collapse></Collapse>
+            </div>
+        </div>
+        </Content>
+        </Layout>
         </div>
     )
+ }
 }
 
 export default Home;
